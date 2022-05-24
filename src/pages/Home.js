d62d80c5bb77bf81ef110f4eb556605b4141356a
Eric  boyer <ericboyer@MacBook-Pro-de-Eric.local>
2022-05-24 12:25:56 +0200

    troisieme commit-happy-cow-probleme json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+
+
+import data from "..assets/data.json";
 
 const Home = () => {
-    
-}
+  console.log(data);
+  return <div>gggf</div>;
+};
 
 export default Home;
