f189a805680058afc405218e77f492942dd2f534
Eric  boyer <ericboyer@MacBook-Pro-de-Eric.local>
2022-05-24 15:30:35 +0200

    quatrieme commit-happy-cow-probleme-daffichage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
-
-
-import data from "..assets/data.json";
+import data from "../assets/data.json";
 
 const Home = () => {
-  console.log(data);
-  return <div>gggf</div>;
+  // console.log(data);
+  return (
+    <div>
+      {data.map((mainname, index) => {
+        console.log(mainname);
+        return (
+          <div key={index}>
+            <p>{mainname.name}</p>
+            {mainname.pictures.map((picture, index) => {
+              return <img key={index} src={picture[0]} alt="" />;
+            })}
+          </div>
+        );
+      })}
+    </div>
+   
+  );
 };
 
 export default Home;
