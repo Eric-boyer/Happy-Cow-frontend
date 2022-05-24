da2eba94b077beb6c0ae3f1693fffe7bf92d0124
Eric  boyer <ericboyer@MacBook-Pro-de-Eric.local>
2022-05-24 16:50:11 +0200

    cinquieme commit-happy-cow-succès

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
 import data from "../assets/data.json";
 
 const Home = () => {
+    
   // console.log(data);
   return (
+      
     <div>
       {data.map((mainname, index) => {
         console.log(mainname);
         return (
           <div key={index}>
-            <p>{mainname.name}</p>
-            {mainname.pictures.map((picture, index) => {
-              return <img key={index} src={picture[0]} alt="" />;
-            })}
+              <p>{mainname.name}</p>
+            <img src={mainname.thumbnail}/>
+            <p>{mainname.description}</p>
+            {/* {mainname.pictures.map((picture, index) => {
+              return <img key={index} src={} alt="" />;
+            })} */}
+           
           </div>
         );
       })}
