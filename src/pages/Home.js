4683532ac3d00e5e756a4c65e06e94fe1a188e68
Eric  boyer <ericboyer@MacBook-Pro-de-Eric.local>
2022-05-25 16:22:46 +0200

    huitieme commit-happy-cow-caroussel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,22 @@ import data from "../assets/data.json";
 
 const Home = () => {
   return (
-    <div>
+    <div  className="caroussel-container">
       {data.map((mainname, index) => {
         console.log(mainname);
         return (
-          <div key={index} className="caroussel-container">
-            {/* <p>{mainname.name}</p> */}
-            <img className="thumbnain-picture" src={mainname.thumbnail} />
+          <div key={index}>
+            <div className="name-thumbail-description-container">
+            <div className="name-thumbnail">
+              <p>{mainname.name}</p>
+              <div className="picture-div">
+              <img className="thumbnain-picture" src={mainname.thumbnail} />
+              </div>
+            </div>
+            <div>
+              <p className="description">{mainname.description}</p>
+            </div>
+          </div>
           </div>
         );
       })}
